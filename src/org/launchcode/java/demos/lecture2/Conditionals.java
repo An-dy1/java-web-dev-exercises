ee02e0bd6593c2749b0f53d9b40e45fce0ee022e
Andie Johnson <ajohnson2@naic.org>
2022-03-10 09:33:36 -0600

    starting point of lecture

--- src/org/launchcode/java/demos/lecture2/Conditionals.java
+++ src/org/launchcode/java/demos/lecture2/Conditionals.java
@@ -1,4 +1,15 @@
 package org.launchcode.java.demos.lecture2;
 
+import java.util.Scanner;
+
 public class Conditionals {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // let's assume we don't have to do validation bc our user will follow the directions carefully:
+        System.out.println("What is your preferred thermostat temperature? Enter a whole number between 50 and 100:");
+        Integer thermo = input.nextInt();
+    }
 }
