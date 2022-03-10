06d28822bc954c825a4170877c38d81c19d63e6f
Andie Johnson <ajohnson2@naic.org>
2022-03-10 10:41:36 -0600

    loops

--- src/org/launchcode/java/demos/lecture2/Conditionals.java
+++ src/org/launchcode/java/demos/lecture2/Conditionals.java
@@ -9,7 +9,14 @@ public class Conditionals {
         Scanner input = new Scanner(System.in);
 
         // let's assume we don't have to do validation bc our user will follow the directions carefully:
-        System.out.println("What is your preferred thermostat temperature? Enter a whole number between 50 and 100:");
-        Integer thermo = input.nextInt();
+        System.out.println("What is your preferred thermostat temperature? Enter a whole number between 50 and 90:");
+        int thermo = input.nextInt();
+
+        // temp between 50-60: positively polar
+        // temp between 60-70: cool as a cucumber
+        // temp between 70-80: tropical times
+        // temp between 80-90: hot and humid
+
+        input.close();
     }
 }
