2d05daf00bb41b3c85cb13e2651f5256619e9649
Andie Johnson <ajohnson2@naic.org>
2022-03-10 09:20:59 -0600

    studio bonus solution

--- src/org/launchcode/java/studios/circlearea/Area.java
+++ src/org/launchcode/java/studios/circlearea/Area.java
@@ -7,15 +7,39 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("What's the radius of your circle? \n");
-        Double radius = input.nextDouble();
-        if(radius < 0 ) {
-            System.out.println("");
-        }
+        System.out.println("What's the radius of your circle?");
 
-        Double area = Circle.getArea(radius);
+        double radius;
+        do {
+            System.out.println("Please enter a positive decimal:");
+            while(!input.hasNextDouble()) {
+                System.out.println("That is not a decimal, enter another choice:");
+                input.next();
+            }
+            radius = input.nextDouble();
+        } while(radius <= 0);
 
-        System.out.println("The area of your circle with radius " + radius + " is: " + area);
+        System.out.println("Your radius is: " + radius);
         input.close();
+
+
     }
 }
+
+
+
+//System.out.println("Please enter a positive decimal for your radius: ");
+//        while(!input.hasNextDouble()){
+//        System.out.println("That isn't a decimal, enter another choice");
+//        input.next();
+//    }
+//    radius = input.nextDouble();
+//
+//        while(radius <= 0) {
+//        System.out.println("That is a negative number, please choose again: ");
+//        radius = input.nextDouble();
+//    }
+//
+//        System.out.println("Your radius is: " + radius);
+//        input.close();
+//}
